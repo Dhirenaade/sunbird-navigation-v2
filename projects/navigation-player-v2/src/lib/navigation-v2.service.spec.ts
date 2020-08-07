ac02807c88fb22ae16d360eb89fab008d8b06c9b
Rahul Shukla <rahul.shukla13@gmail.com>
2020-08-07 15:31:50 +0530

    service fix

--- projects/navigation-player-v2/src/lib/navigation-v2.service.spec.ts
+++ projects/navigation-player-v2/src/lib/navigation-v2.service.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { PdfPlayerV2Service } from './pdf-player-v2.service';
+import { NavigationV2Service } from './navigation-v2.service';
 
 describe('PdfPlayerV2Service', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: PdfPlayerV2Service = TestBed.get(PdfPlayerV2Service);
+    const service: NavigationV2Service = TestBed.get(NavigationV2Service);
     expect(service).toBeTruthy();
   });
 });
+
--- projects/navigation-player-v2/src/lib/navigation-v2.service.ts
+++ projects/navigation-player-v2/src/lib/navigation-v2.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class PdfPlayerV2Service {
+export class NavigationV2Service {
 
   constructor() { }
 }
